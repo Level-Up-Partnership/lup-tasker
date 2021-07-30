60ebde3b36b34e7ab33dc614b13e85fce38ed16b
hadifayad9 <hadifayad9@hotmail.com>
2021-07-29 20:42:30 -0400

    TS-21 Created REG Form

---
+++ client/src/components/RegForm/RegistrationForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="displayedName">Display Name: </label>
+        <input type="text" id="displayedName" />
+      </div>
+      <div class="form-group">
+        <label for="userEmail">Email: </label>
+        <input type="email" id="userEmail" class="form-group" />
+      </div>
+      <div class="form-group">
+        <label for="userPassword">Password</label>
+        <input type="password" id="userPassword" placeholder="Enter Password" />
+      </div>
+      <div class="form-group">
+        <label for="userConfirmedPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="userConfirmedPassword"
+          placeholder="Confirm Password"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+</style>
