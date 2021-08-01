72e1c28d9e045bcbbfdb1c98b65fe8afcfdf0fd2
hadifayad9 <hadifayad9@hotmail.com>
2021-08-01 13:04:14 -0400

    Used emits to talk to parent component | Parent component uses axios to add user

--- client/src/components/RegForm/RegistrationForm.vue
+++ client/src/components/RegForm/RegistrationForm.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="submitData">
       <div class="form-group">
         <label for="displayedName">Display Name: </label>
-        <input type="text" id="displayedName" />
+        <input type="text" id="displayedName" v-model="enteredName" />
       </div>
       <div class="form-group">
         <label for="userEmail">Email: </label>
-        <input type="email" id="userEmail" class="form-group" />
+        <input
+          type="email"
+          id="userEmail"
+          class="form-group"
+          v-model="enteredEmail"
+        />
       </div>
       <div class="form-group">
         <label for="userPassword">Password</label>
-        <input type="password" id="userPassword" placeholder="Enter Password" />
+        <input
+          type="password"
+          id="userPassword"
+          placeholder="Enter Password"
+          v-model="enteredPassword"
+        />
       </div>
       <div class="form-group">
         <label for="userConfirmedPassword">Confirm Password</label>
@@ -21,14 +31,33 @@
           placeholder="Confirm Password"
         />
       </div>
+      <div>
+        <button>Register</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      enteredName: "",
+      enteredPassword: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-user",
+        this.enteredName,
+        this.enteredPassword,
+        this.enteredEmail
+      );
+    },
+  },
+};
 </script>
 
-
-<style scoped>
-</style>
+<style scoped></style>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
     <h1>Register</h1>
-    <registration-form></registration-form>
+    <registration-form @add-user="userInfo"></registration-form>
   </div>
 </template>
 
 <script>
 import RegistrationForm from "../components/RegForm/RegistrationForm.vue";
+import axios from "axios";
 export default {
   components: { RegistrationForm },
+  data() {
+    return {
+      newUser: [],
+    };
+  },
+  methods: {
+    async userInfo(name, password, email) {
+      const newUser = {
+        name: name,
+        password: password,
+        email: email,
+      };
+      console.log(newUser);
+      this.newUser = newUser;
+      await axios.post("http://localhost:5000/register", {
+        username: this.newUser.name,
+        password: this.newUser.password,
+        email: this.newUser.email,
+      });
+    },
+  },
 };
 </script>
 
