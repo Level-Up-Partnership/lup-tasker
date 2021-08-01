3d7e2c207bbe2f9a9ce030f894e8504cc52050cc
hadifayad9 <hadifayad9@hotmail.com>
2021-08-01 11:07:14 -0400

    TAS-8 Connected database to backend/backend to frontend using axios

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>Test login</div>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ users.username }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get("http://localhost:5000/users/");
+      this.users = res.data;
+      console.log(this.users[0].username);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
 
 <style scoped>
