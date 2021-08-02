bd81f93776edbd7e5cf8b8001889f86531bfe630
hadifayad9 <hadifayad9@hotmail.com>
2021-08-01 22:20:04 -0400

    Added bcrypt to hash passwords in DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors')
-const bcrypt = require('bcrypt');
 const app = express();
 
 //MIDDLEWARE
--- server/routes/api/register.js
+++ server/routes/api/register.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const client = require('../../connection/pg')
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
     try {
-        const { username, password, email } = req.body
+        const { username, email } = req.body;
+        const password = bcrypt.hashSync(req.body.password, 10);
         const newUser = await client.query(`INSERT INTO taskeruser (username,password,email) VALUES ('${username}','${password}','${email}') RETURNING *`);
         res.json(newUser);
     } catch (error) {
