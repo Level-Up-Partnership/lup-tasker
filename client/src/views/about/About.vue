6d8fe1a63150d0ef31d415a680a34f16928b8e68
hadifayad9 <hadifayad9@hotmail.com>
2021-08-01 22:33:25 -0400

    TAS-37 about page created

---
+++ client/src/views/about/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>ABOUT</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ad tenetur
+      sint culpa aliquid, quibusdam adipisci dolor nemo quia, laboriosam
+      reprehenderit consequatur eveniet nisi. Quidem architecto laboriosam
+      tempora quos suscipit!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
