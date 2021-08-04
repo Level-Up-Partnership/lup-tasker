82b9de9835e981f3a429389a9a4cce9a7f6d26b6
hadifayad9 <hadifayad9@hotmail.com>
2021-08-04 12:01:46 -0400

    Added JWT tokens

--- server/routes/api/loginRoute/login.js
+++ server/routes/api/loginRoute/login.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const client = require('../../../connection/pg')
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 const JWT = require('jsonwebtoken')
 
 router.post('/', async (req, res) => {
     try {
         const email = req.body.email;
-        const dbEmail = await client.query(`SELECT email,password FROM taskeruser WHERE email = '${email}'`);
+        const dbEmail = await client.query(`SELECT user_id,email,password FROM taskeruser WHERE email = '${email}'`);
         if (dbEmail.rowCount === 0) {
             return res.status(401).json({
                 title: 'User does not exist',
@@ -20,8 +21,11 @@ router.post('/', async (req, res) => {
                 error: 'invalid password'
             });
         }
-        console.log("Logged in");
-        let token = JWT.sign()
+        let token = JWT.sign({ userId: dbEmail.rows[0].user_id, }, process.env.JWT_SECRET)
+        return res.status(200).json({
+            title: 'Logged in',
+            token: token
+        })
     } catch (error) {
         console.log(error);
     }
