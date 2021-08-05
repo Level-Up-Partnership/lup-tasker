20a7d2ba82909f1605f3823f614aecd21e825e94
hadifayad9 <hadifayad9@hotmail.com>
2021-08-04 22:37:46 -0400

    Added homepage that checks if the user has a token

---
+++ client/src/views/homepage/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="logout">logout</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  created() {
+    if (localStorage.getItem("token") === null) {
+      this.$router.push("/login");
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
