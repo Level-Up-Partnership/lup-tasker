f8a1a972c838e21b8cd17d778322b93bcd6102e9
hadifayad9 <hadifayad9@hotmail.com>
2021-08-08 19:32:47 -0400

    Created vuex store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex";
+import authModule from "./auth/index.js";
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    auth: authModule,
+  },
 });
