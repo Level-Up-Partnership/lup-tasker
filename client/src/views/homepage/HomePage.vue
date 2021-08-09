75209d35f8c7df705aed5f73c91419419f7b736a
hadifayad9 <hadifayad9@hotmail.com>
2021-08-08 20:29:25 -0400

    Gets userinfo from backend based on token and dispays it

--- client/src/views/homepage/HomePage.vue
+++ client/src/views/homepage/HomePage.vue
@@ -1,11 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Username: {{ userName }}</h1>
+    email : {{ email }}
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  data() {
+    return {
+      userName: "",
+      email: "",
+    };
+  },
   created() {
     this.$store.dispatch("CheckIfLoggedIn");
+  },
+  mounted() {
+    axios
+      .get("/user", { headers: { token: localStorage.getItem("token") } })
+      .then((res) => {
+        console.log(res);
+        this.userName = res.data.userInfo.username;
+        this.email = res.data.userInfo.email;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
