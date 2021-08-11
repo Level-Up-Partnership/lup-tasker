38553be3ea11197644c99d9e0d98bc95ba880414
hadifayad9 <hadifayad9@hotmail.com>
2021-08-10 23:04:33 -0400

    Fixed up NotFound page. Now returns the users URL they searched for

--- client/src/views/notfound/NotFound.vue
+++ client/src/views/notfound/NotFound.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    <h1>Page not Found</h1>
+    <div class="container">
+      <div class="d-flex align-items-start flex-column">
+        <h1 class="title">Tasker</h1>
+        <h2 class="notfoundcode">404</h2>
+        <p>
+          The request URL {{ userUrl }} was not found on this server
+          <router-link to="/homepage">Click here</router-link>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userUrl: this.$route.fullPath,
+    };
+  },
+  mounted() {
+    this.$store.dispatch("CheckIfLoggedIn");
+  },
+};
+</script>
+
+
+<style scoped>
+.title {
+  font-size: 100px;
+}
+.notfoundcode {
+  font-size: 50px;
+}
+</style>
