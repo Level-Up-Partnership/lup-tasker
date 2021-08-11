d687b4ebdf4e0c58081083679f16e00c01f2363b
hadifayad9 <hadifayad9@hotmail.com>
2021-08-10 23:04:48 -0400

    Tested out some colum stuff for bootstrap

--- client/src/views/userProfile/UserProfile.vue
+++ client/src/views/userProfile/UserProfile.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h1>USER PROFILE</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">USER PROFILE</div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-export default {};
+export default {
+  mounted() {
+    this.$store.dispatch("CheckIfLoggedIn");
+  },
+};
 </script>
 
 
