af09d0d388a00dd2545e6240ddcba7138e751daf
hadifayad9 <hadifayad9@hotmail.com>
2021-08-31 21:19:43 -0400

    Axios get call that calls the API route which fetches data from the database and displays it

--- client/src/views/forum/mainForum.vue
+++ client/src/views/forum/mainForum.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <h1>Discussion Forum</h1>
-    <ul>
+    <ul v-if="isLoaded">
       <li>
-        <router-link to="/forum/categorysuggestion"
-          >Category Suggestion</router-link
-        >
+        <router-link to="/forum/categorysuggestion">{{
+          CategoryName[0].title
+        }}</router-link>
+        <div>{{ CategoryName[0].description }}</div>
       </li>
       <li>
         <router-link to="/forum/generaldiscussion"
-          >General Discussion</router-link
-        >
+          >{{ CategoryName[1].title }}
+        </router-link>
+        <div>{{ CategoryName[1].description }}</div>
       </li>
       <li>
-        <router-link to="/forum/helpdesk">Help Desk</router-link>
+        <router-link to="/forum/helpdesk"
+          >{{ CategoryName[2].title }}
+        </router-link>
+        <div>{{ CategoryName[2].description }}</div>
       </li>
       <li>
-        <router-link to="/forum/social">Social</router-link>
+        <router-link to="/forum/social"
+          >{{ CategoryName[3].title }}
+        </router-link>
+        <div>{{ CategoryName[3].description }}</div>
       </li>
     </ul>
   </div>
@@ -24,10 +32,24 @@
 
 
 <script>
+import axios from "axios";
 export default {
+  data() {
+    return {
+      CategoryName: [],
+      isLoaded: false,
+    };
+  },
   mounted() {
     this.$store.dispatch("CheckIfLoggedIn");
     this.$store.dispatch("CheckUserRole");
+  },
+  async created() {
+    await axios.get("/categoryInfo").then((res) => {
+      //   console.log(res.data);
+      this.CategoryName = res.data;
+    });
+    this.isLoaded = true;
   },
 };
 </script>
