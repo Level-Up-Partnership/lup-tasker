8f046f1701e86b940413661911d1ca8942255360
hadifayad9 <hadifayad9@hotmail.com>
2021-09-02 19:26:22 -0400

    PostCreation page that is displayed when the user clicks on "create post"

---
+++ client/src/views/forum/postCreation.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <post-topic @post-creation="postCreationData"></post-topic>
+    <div>
+      {{ currentRouteName }}
+    </div>
+  </div>
+</template>
+
+<script>
+import PostTopic from "../../components/Forum/PostTopic.vue";
+import axios from "axios";
+export default {
+  components: { PostTopic },
+  data() {
+    return {};
+  },
+  methods: {
+    // async postCreationData(title, description) {
+    //   axios.post("/postCreation");
+    // },
+  },
+  created() {
+    console.log(this.$route.name);
+  },
+  computed: {
+    currentRouteName() {
+      return this.$route.name;
+    },
+  },
+};
+</script>
+
+
+<style>
+</style>
