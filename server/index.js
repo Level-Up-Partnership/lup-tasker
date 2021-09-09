5f5824e287d75b92969d7dde866ea44810e0df58
hadifayad9 <hadifayad9@hotmail.com>
2021-09-09 13:21:31 -0400

    changed dist location

--- server/index.js
+++ server/index.js
@@ -29,11 +29,11 @@ app.use('/getPosts', require('./routes/api/ForumAPI/GetForumPost'));
 //     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 // }
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '../client/dist')))
+app.use('/', serveStatic(path.join(__dirname, '../client/dist/tasker-app')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../client/dist/tasker-app/index.html'))
 })
 const PORT = process.env.PORT || 5000;
 
