8194e2264fca79a6d305145a42b196aa78fe600e
hadifayad9 <hadifayad9@hotmail.com>
2021-09-08 20:39:50 -0400

    updated index

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use('/getPosts', require('./routes/api/ForumAPI/GetForumPost'));
 
 if (process.env.NODE_ENV === 'production') {
     //
-    app.use(express.static(__dirname + '/public'))
+    app.use(express.static(__dirname + '/public/'))
 
     //Handle spa
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
