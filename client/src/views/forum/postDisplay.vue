90fb6c637217b22a5327998bd2bfdc4944f8e836
hadifayad9 <hadifayad9@hotmail.com>
2021-09-11 16:34:29 -0400

    Changed whole logic to require one categoryid to create a post

--- client/src/views/forum/postDisplay.vue
+++ client/src/views/forum/postDisplay.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <h1>Post</h1>
+  <div v-if="isLoaded">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  created() {
-    console.log(this.$route.params.id);
+  data() {
+    return {
+      title: "",
+      description: "",
+      isLoaded: false,
+    };
+  },
+  async created() {
+    await axios
+      .get("/getPosts", {
+        headers: {
+          token: localStorage.getItem("token"),
+          forumid: this.$route.params.id,
+        },
+      })
+      .then((res) => {
+        this.title = res.data[0].title;
+        this.description = res.data[0].description;
+        this.isLoaded = true;
+      });
   },
 };
 </script>
