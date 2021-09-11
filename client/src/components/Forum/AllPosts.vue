f739120fae844276102a82eb93b10c003a7a50c1
hadifayad9 <hadifayad9@hotmail.com>
2021-09-10 22:21:46 -0400

    Component that holds an array of user posts

---
+++ client/src/components/Forum/AllPosts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ol class="list-group">
+      <li
+        class="list-group-item d-flex justify-content-between align-items-start"
+      >
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">
+            <router-link :to="`/forum/categorysuggestion/post/${fourmId}`">{{
+              forumTitle
+            }}</router-link>
+          </div>
+          {{ forumDescription }}
+        </div>
+        <span class="badge bg-primary rounded-pill">{{ createdAt }}</span>
+      </li>
+    </ol>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ["fourmId", "forumTitle", "forumDescription", "createdAt"],
+};
+</script>
+
+
+<style scoped>
+h2 {
+  cursor: pointer;
+  text-decoration: underline;
+  color: #1a0dab;
+}
+</style>
