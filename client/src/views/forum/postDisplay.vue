2e8d6a14641e1aed68515035d10c39a558d83811
hadifayad9 <hadifayad9@hotmail.com>
2021-09-10 22:23:19 -0400

    Will be what the post is displayed. Going to use route param to decide which post to get from backend

---
+++ client/src/views/forum/postDisplay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Post</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route.params.id);
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
