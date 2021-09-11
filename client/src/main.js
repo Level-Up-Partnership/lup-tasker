fb5f6aced98735742882edd51eec64f46af438e4
hadifayad9 <hadifayad9@hotmail.com>
2021-09-10 22:21:29 -0400

    added axios globally

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import BaseCard from "./components/UI/BaseCard.vue";
 import BaseButton from "./components/UI/BaseButton.vue";
-
+import './axios';
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
