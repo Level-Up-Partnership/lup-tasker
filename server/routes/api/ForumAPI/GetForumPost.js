d7aa8044662b6731d55d2febddb4676728f9f391
hadifayad9 <hadifayad9@hotmail.com>
2021-09-11 16:32:07 -0400

    Added a join to get the title

--- server/routes/api/ForumAPI/GetForumPost.js
+++ server/routes/api/ForumAPI/GetForumPost.js
@@ -8,18 +8,38 @@ const JWT = require('jsonwebtoken')
 
 router.get('/', async (req, res, next) => {
     let token = req.headers.token;
-    let route = req.headers.route;
-
+    let forumid = req.headers.forumid;
     JWT.verify(token, process.env.JWT_SECRET, async (err) => {
         if (err) return res.status(401).json({
             title: 'unauthroized'
         })
-        const categoryPosts = await client.query(`SELECT * FROM forumpost where categoryid = ${req.headers.categoryid}`).catch(err => {
-            if (err) {
-                console.log(err);
+        if (req.headers.categoryid) {
+
+            const categoryPosts = await client.query(`SELECT * FROM forumpost where categoryid = ${req.headers.categoryid}`).catch(err => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            const categoryTitle = await client.query(`SELECT DISTINCT category.title
+            FROM forumpost 
+            INNER JOIN category ON forumpost.categoryid = category.categoryid
+            WHERE category.categoryid = ${req.headers.categoryid}`);
+
+            const userPost = {
+                title: categoryTitle.rows,
+                userPosts: categoryPosts.rows
             }
-        })
-        return res.json(categoryPosts.rows);
+
+
+            return res.json(userPost);
+        } else {
+            const userPost = await client.query(`SELECT * FROM forumpost where forumpostId = ${forumid}`).catch(err => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            return res.json(userPost.rows);
+        }
     });
 });
 
