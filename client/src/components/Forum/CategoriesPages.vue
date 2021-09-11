a0af26129bf802211d9433cb0faaa5c723ab13fa
hadifayad9 <hadifayad9@hotmail.com>
2021-09-11 16:03:57 -0400

    displays my category pages component

---
+++ client/src/components/Forum/CategoriesPages.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="row justify-content-center">
+      <ol class="list-group col-6 text-left">
+        <li class="list-group-item d-flex align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold text-start">
+              <router-link
+                :to="`/forum/category/${categoryId}/`"
+                class="align-items-start"
+                >{{ categoryTitle }}</router-link
+              >
+            </div>
+            <div>
+              <p class="text-start">
+                {{ categoryDescription }}
+              </p>
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ["categoryId", "categoryTitle", "categoryDescription"],
+};
+</script>
+
+
+<style scoped>
+</style>
