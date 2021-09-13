a527872bc04991216d0bded81b450f6aceea7a44
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 13:27:14 -0400

    addCategory route that inserts a new category when called

---
+++ server/routes/api/addCategory/addCategory.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../../../connection/pg')
+require('dotenv').config();
+const JWT = require('jsonwebtoken')
+
+
+router.post('/', async (req, res) => {
+    JWT.verify(req.body.token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(401).json({
+            title: 'unauthroized'
+        })
+        const description = req.body.description;
+        const descriptionSingleQuotes = description.replace(/'/g, "''");
+        await client.query(`INSERT INTO category (title,description) VALUES 
+        ('${req.body.title}','${descriptionSingleQuotes}')`).catch(err => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        return res.status(200).json({
+            title: 'Category has been Created!!',
+        })
+    });
+});
+
+
+module.exports = router;
