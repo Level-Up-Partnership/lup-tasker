41af7c8bdec07827f38e3451233b6e68866a80f9
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 15:10:22 -0400

    .

--- server/routes/api/ForumAPI/GetForumPost.js
+++ server/routes/api/ForumAPI/GetForumPost.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const client = require('../../../connection/pg')
-const bcrypt = require('bcrypt');
 require('dotenv').config();
 const JWT = require('jsonwebtoken')
 
