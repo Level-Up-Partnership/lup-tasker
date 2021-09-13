878e9584916644cac08c05a16b3eee098f9307a8
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 13:26:30 -0400

    Changed the way the user profile looks

--- client/src/components/UserProfile/UserProfileComp.vue
+++ client/src/components/UserProfile/UserProfileComp.vue
@@ -4,76 +4,83 @@
     <div class="img-container">
       <img src="../../assets/user/index.png" alt="" />
     </div>
-    <form @submit.prevent="changePassword">
-      <div class="d-flex align-items-start">
-        <h3>Change Password</h3>
+    <base-card v-if="addCategoryAction">
+      <div class="col">
+        <form @submit.prevent="changePassword">
+          <div>
+            <h3>Change Password</h3>
+          </div>
+          <div class="form-group">
+            <label for="currentPassword">Current Password</label>
+            <input
+              type="password"
+              v-model="currentPassword"
+              class="form-control"
+              id="currentPassword"
+              placeholder="Current Password"
+            />
+          </div>
+          <div class="form-group">
+            <label for="newPassword">New Password</label>
+            <input
+              type="password"
+              v-model="newPassword"
+              class="form-control"
+              id="newPassword"
+              placeholder="New Password"
+            />
+          </div>
+          <div class="form-group">
+            <label for="confirmPassword">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="confirmPassword"
+              placeholder="Confirm Password"
+            />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-dark button-form">Change Password</button>
+          </div>
+        </form>
       </div>
+    </base-card>
+    <admin-category v-if="!addCategoryAction"></admin-category>
+    <div class="container">
       <div class="row">
-        <label for="currentPassword" class="col-sm-2 col-form-label"
-          >Current Password</label
-        >
-        <div class="col-sm-2">
-          <input
-            type="password"
-            v-model="currentPassword"
-            class="form-control"
-            id="currentPassword"
-            placeholder="Current Password"
-          />
+        <div v-if="userRole === 1" class="col">
+          <button class="btn btn-dark">Change Banner</button>
+        </div>
+        <div v-if="userRole === 1" class="col" @click="addCategory">
+          <button class="btn btn-dark">Add Category</button>
         </div>
       </div>
-      <div class="row">
-        <label for="newPassword" class="col-sm-2 col-form-label"
-          >New Password</label
-        >
-        <div class="col-sm-2">
-          <input
-            type="password"
-            v-model="newPassword"
-            class="form-control"
-            id="newPassword"
-            placeholder="New Password"
-          />
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="confirmPassword" class="col-sm-2 col-form-label"
-          >Password</label
-        >
-        <div class="col-sm-2">
-          <input
-            type="password"
-            class="form-control"
-            id="confirmPassword"
-            placeholder="Confirm Password"
-          />
-        </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-sm-10">
-          <button class="btn btn-dark button-form">Change Password</button>
-        </div>
-      </div>
-    </form>
-    <div v-if="userRole === 1" class="admin-button">
-      <button class="btn btn-dark">Change Banner</button>
     </div>
   </div>
 </template>
 
 <script>
+import AdminCategory from "../UserProfile/AdminCategory.vue";
 export default {
+  components: {
+    AdminCategory,
+  },
   data() {
     return {
       currentPassword: "",
       newPassword: "",
+      addCategoryAction: true,
     };
   },
   methods: {
     changePassword() {
       this.$emit("change-password", this.newPassword, this.currentPassword);
     },
+    addCategory() {
+      this.addCategoryAction = !this.addCategoryAction;
+    },
   },
+
   computed: {
     userRole() {
       return this.$store.getters.UserRole;
@@ -83,34 +90,4 @@ export default {
 </script>
 
 <style scoped>
-.form-control-inline {
-  display: inline;
-}
-form {
-  margin: 0;
-  position: absolute;
-  right: 10%;
-  bottom: 62%;
-  width: 70%;
-}
-.button-form {
-  margin: 0;
-  position: relative;
-  bottom: -60%;
-  right: 40%;
-}
-.img-container img {
-  margin-top: 2%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-.admin-button {
-  margin: 0;
-  position: absolute;
-  width: 500px;
-  height: 50px;
-  right: 45%;
-  bottom: 25%;
-}
 </style>
