8911fb7a08e7553b70fc457266b35c5ce5201132
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 15:11:01 -0400

    Delete route that deletes first the posts inside the category before being able to delete the category itself (admin only)

---
+++ server/routes/api/ForumAPI/DeleteCategory.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const JWT = require('jsonwebtoken')
+const client = require('../../../connection/pg')
+
+router.delete('/', async (req, res) => {
+    let token = req.headers.token;
+    let categoryid = req.headers.categoryid;
+    console.log(categoryid);
+    JWT.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(401).json({
+            title: 'unauthroized'
+        })
+        await client.query(`DELETE FROM forumpost where categoryid=${categoryid}`)
+        await client.query(`DELETE FROM category where categoryid=${categoryid}`)
+
+        return res.status(200).json({
+            title: 'Category has been deleted',
+        })
+    });
+});
+
+
+module.exports = router;
