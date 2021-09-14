5d4cabf75d1358d79da22b65f580c1850dded323
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 21:20:26 -0400

    Simple route to delete a post the user created

---
+++ server/routes/api/ForumAPI/DeletePost.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const JWT = require('jsonwebtoken')
+const client = require('../../../connection/pg')
+
+router.delete('/', async (req, res) => {
+    let token = req.headers.token;
+    let forumid = req.headers.forumid;
+    console.log(forumid);
+    JWT.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(401).json({
+            title: 'unauthroized'
+        })
+        await client.query(`DELETE FROM forumpost where forumpostId=${forumid}`)
+
+        return res.status(200).json({
+            title: 'Post has been deleted',
+        })
+    });
+});
+
+
+module.exports = router;
