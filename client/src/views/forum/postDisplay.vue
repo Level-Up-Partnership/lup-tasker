e5d3f3e27ba3dca4f9b1e7693bcf529d5ff84e9d
hadifayad9 <hadifayad9@hotmail.com>
2021-09-13 21:19:54 -0400

    Grab the currentUserId logged in. Check if the userLogged in posted the forum post. If not dont show the delete button. If so show button and let them delete with a simple axios call to backend to delete post depending on the ID.

--- client/src/views/forum/postDisplay.vue
+++ client/src/views/forum/postDisplay.vue
@@ -2,6 +2,9 @@
   <div v-if="isLoaded">
     <h1>{{ title }}</h1>
     <p>{{ description }}</p>
+    <button @click="deletePost" v-if="userIdPost === currentUserId">
+      DELETE
+    </button>
   </div>
 </template>
 
@@ -12,6 +15,10 @@ export default {
     return {
       title: "",
       description: "",
+      forumId: null,
+      categoryId: null,
+      userIdPost: null,
+      currentUserId: null,
       isLoaded: false,
     };
   },
@@ -24,10 +31,33 @@ export default {
         },
       })
       .then((res) => {
-        this.title = res.data[0].title;
-        this.description = res.data[0].description;
+        console.log(res);
+        this.title = res.data.userPostInfo[0].title;
+        this.categoryId = res.data.userPostInfo[0].categoryid;
+        this.description = res.data.userPostInfo[0].description;
+        this.forumId = res.data.userPostInfo[0].forumpostid;
+        this.userIdPost = res.data.userPostInfo[0].userid;
+        this.currentUserId = res.data.currentUserid;
         this.isLoaded = true;
       });
+  },
+  methods: {
+    async deletePost() {
+      await axios
+        .delete("/deletePost", {
+          headers: {
+            token: localStorage.getItem("token"),
+            forumid: this.forumId,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.$router.push(`/forum/category/${this.categoryId}/`);
+    },
   },
 };
 </script>
