7e6798a4aebb3278af39335f58f229d2cbb64127
hadifayad9 <hadifayad9@hotmail.com>
2021-09-16 10:05:11 -0400

    Going to hold my array of topic replies

---
+++ client/src/components/Forum/ForumPostReplies.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <h2>{{ userName }}</h2>
+        </li>
+        <h3>{{ userReply }}</h3>
+        <p>{{ createdAt }}</p>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["topicReplyId", "userName", "userReply", "createdAt"],
+};
+</script>
+
+
+<style scoped>
+.list-group {
+  display: inline-block;
+  width: 50%;
+}
+</style>
