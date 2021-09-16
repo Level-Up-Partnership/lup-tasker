87ead36e0366aac0bd5227e325f01e98cc5c2475
hadifayad9 <hadifayad9@hotmail.com>
2021-09-16 10:06:21 -0400

    Topic reply view and component for posting the topic

---
+++ client/src/views/forum/postReply.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <post-topic @post-creation="postCreationData"></post-topic>
+  </div>
+</template>
+
+<script>
+import PostTopic from "../../components/Forum/PostTopic.vue";
+import axios from "axios";
+export default {
+  components: { PostTopic },
+  data() {
+    return {
+      categoryId: this.$route.params.id,
+    };
+  },
+  methods: {
+    async postCreationData(title, description) {
+      //   axios
+      //     .post("/postCreation", {
+      //       title: title,
+      //       description: description,
+      //       token: localStorage.getItem("token"),
+      //       category: this.categoryId,
+      //     })
+      //     .then((res) => {
+      //       console.log(res);
+      //     });
+      ////forum/${categoryName}/post/${fourmId}
+      this.$router.push(`/forum/category/${this.categoryId}/`);
+    },
+  },
+};
+</script>
+
+
+<style>
+</style>
