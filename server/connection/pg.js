aff541f799aeabbe0f5b7271678b06b9995a543f
hadifayad9 <hadifayad9@hotmail.com>
2021-09-19 21:54:51 -0400

    .

--- server/connection/pg.js
+++ server/connection/pg.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-    connectionString: process.env.CONNECTION,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
