c6bbde65ed0ca9e2470c8c78bb08404bc3538f29
hadifayad9 <hadifayad9@hotmail.com>
2021-09-21 22:33:20 -0400

    Creates the task in the database

---
+++ server/routes/api/TaskAPI/createTask/createTask.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../../../../connection/pg')
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const JWT = require('jsonwebtoken')
+
+
+router.post('/', async (req, res) => {
+    JWT.verify(req.body.token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(401).json({
+            title: 'unauthroized'
+        })
+        //Creates orginial Task, VALUES are determined 
+        //When user starts Task, hold the amount of times the pomodoro timer is pressed, so if its 2 cycles, 3 million miliseconds. when the user clicks done,
+        //EVERYTIME the user presses "Start timer" a total timer will run along the regular pomodoro timer that will be used to add the totalFocusTimer, that will be
+        // the Main focus timer. If the user stops at a certain point it will get that amount in miliseconds.
+        await client.query(`INSERT INTO tasks (taskname,category,iscomplete,focustimer,resttimer,userid) VALUES 
+        ('${req.body.taskName}','${req.body.taskCategory}',${req.body.isComplete},${req.body.focusTimer},${req.body.restTimer},'${decoded.userId}')`)
+
+        return res.status(200).json({
+            title: 'Success',
+        })
+    });
+});
+
+
+module.exports = router;
