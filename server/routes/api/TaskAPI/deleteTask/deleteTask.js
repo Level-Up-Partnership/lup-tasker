01dd813225c093805731ba2c81b4869c6367c349
hadifayad9 <hadifayad9@hotmail.com>
2021-10-01 22:21:01 -0400

    Delete a task by id route

---
+++ server/routes/api/TaskAPI/deleteTask/deleteTask.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const JWT = require('jsonwebtoken')
+const client = require('../../../../connection/pg')
+
+router.delete('/', async (req, res) => {
+    let token = req.headers.token;
+    let taskid = req.headers.taskid;
+    JWT.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(401).json({
+            title: 'unauthroized'
+        })
+        await client.query(`DELETE FROM tasks where taskid=${taskid}`)
+
+        return res.status(200).json({
+            title: 'Post has been deleted',
+        })
+    });
+});
+
+
+module.exports = router;
