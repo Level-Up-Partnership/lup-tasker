cc5cb70d2b35ba039ceea30da25f88a4bc182116
hadifayad9 <hadifayad9@hotmail.com>
2021-10-04 20:15:11 -0400

    update axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
 
 //uncomment this and build your app when deploying
-axios.defaults.baseURL = 'http://localhost:5000/';
+if (process.env.NODE_ENV === 'development') {
+    axios.defaults.baseURL = 'http://localhost:5000/';
+}
