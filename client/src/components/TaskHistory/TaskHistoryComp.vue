ce0d445ac5c61a97c6315fe0ab1b88d572ce0a51
hadifayad9 <hadifayad9@hotmail.com>
2021-10-08 22:54:24 -0400

    Task component that'll hold the tasks

---
+++ client/src/components/TaskHistory/TaskHistoryComp.vue
@@ -0,0 +1,92 @@
+<template>
+  <base-card>
+    <div>
+      <div>
+        <div class="position-absolute top-0 end-0">
+          <button
+            type="button"
+            class="btn-close"
+            aria-label="Close"
+            @click="deleteTask"
+          ></button>
+        </div>
+        <div class="position-absolute top-0 start-0 category">
+          Category: {{ category }}
+        </div>
+        <h1>{{ taskName }}</h1>
+      </div>
+      <div id="timer">
+        <span id="minutes">{{ minutes }}</span>
+        <span id="middle">:</span>
+        <span id="seconds">{{ seconds }}</span>
+        <span> Mins Focused Time</span>
+      </div>
+      <div id="timer">
+        <span id="minutes">{{ restMinutes }}</span>
+        <span id="middle">:</span>
+        <span id="seconds">{{ restSeconds }}</span>
+        <span> Mins Rest Time</span>
+      </div>
+      <div id="timer">
+        <span id="minutes">{{ totalTimerMinutes }}</span>
+        <span id="middle">:</span>
+        <span id="seconds">{{ totalTimerSeconds }}</span>
+        <span> Mins Total Time</span>
+      </div>
+    </div>
+  </base-card>
+</template>
+<script>
+export default {
+  props: {
+    taskName: String,
+    category: String,
+    isComplete: Boolean,
+    totalFocusTimer: Number,
+    totalrestTimer: Number,
+    totalTimer: Number,
+    taskId: Number,
+  },
+  methods: {
+    padTime(time) {
+      return (time < 10 ? "0" : "") + time;
+    },
+  },
+  computed: {
+    minutes() {
+      const minutes = Math.floor((this.totalFocusTimer * 60) / 60);
+      return this.padTime(Math.round(minutes));
+    },
+    seconds() {
+      const seconds = this.totalFocusTimer * 60 - this.minutes * 60;
+      return this.padTime(Math.round(seconds));
+    },
+    restMinutes() {
+      const minutes = Math.floor((this.totalrestTimer * 60) / 60);
+      return this.padTime(Math.round(minutes));
+    },
+    restSeconds() {
+      const seconds = this.totalrestTimer * 60 - this.restMinutes * 60;
+      return this.padTime(Math.round(seconds));
+    },
+    totalTimerMinutes() {
+      const minutes = Math.floor((this.totalTimer * 60) / 60);
+      return this.padTime(Math.round(minutes));
+    },
+    totalTimerSeconds() {
+      const seconds = this.totalTimer * 60 - this.totalTimerMinutes * 60;
+      return this.padTime(Math.round(seconds));
+    },
+  },
+};
+</script>
+
+
+
+<style scoped>
+#timer {
+  font-size: 24px;
+  line-height: 1;
+  margin-bottom: 40px;
+}
+</style>
